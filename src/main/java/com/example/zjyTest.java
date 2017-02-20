3750b074da9a511eee99d5f22ce09317ae96553c
yangmei <yangm@daqsoft.com>
2017-02-20 18:37:50 +0800

    zjy第一次提交

--- src/main/java/com/example/zjyTest.java
+++ src/main/java/com/example/zjyTest.java
@@ -4,7 +4,10 @@ package com.example;
  * Created by yangmei on 17/2/20.
  */
 public class zjyTest {
+    String s = "";
     public static void main(String[] args) {
         System.out.println("11111111");
     }
+
+
 }
